1f08c9a55f99c0d45349337b8c3840bf96e475f4
Florencia Soto <florenciasoto801@gmail.com>
2021-05-19 21:16:18 -0300

    Updates

---
+++ precache-manifest.1cc20a56663605aa9ffa48b6a254d408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc00fe28f4d23bc72c98e3d92f38c753",
+    "url": "/Giffy/index.html"
+  },
+  {
+    "revision": "def2c1f87cd85c5f68fd",
+    "url": "/Giffy/static/css/main.c697133a.chunk.css"
+  },
+  {
+    "revision": "0c2461399aa7ca381d0c",
+    "url": "/Giffy/static/js/2.31c80e29.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Giffy/static/js/2.31c80e29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def2c1f87cd85c5f68fd",
+    "url": "/Giffy/static/js/main.5d75f6d4.chunk.js"
+  },
+  {
+    "revision": "0fbfe6033b32afcd3755",
+    "url": "/Giffy/static/js/runtime-main.6c81b059.js"
+  }
+]);
