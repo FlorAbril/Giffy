ef6991dfa0fc1fc94018b42ca80b5d204edc52dd
Florencia Soto <florenciasoto801@gmail.com>
2021-06-05 14:52:23 -0300

    Arreglo errores del filtro de lenguajes

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,67 +1,77 @@
-import React from 'react';
-import { useLocation } from 'wouter';
-import useForm from './hook'
-import './index.css'
+import React from "react";
+import { useLocation } from "wouter";
+import useForm from "./hook";
+import "./index.css";
 
-const RATINGS = ['g', 'pg', 'pg-13', 'r']
-const LANGUAGES = {en:'english',es:'español',pt:'português'}
+const RATINGS = ["g", "pg", "pg-13", "r"];
+const LANGUAGES = { en: "english", es: "español", pt: "português" };
 
-const defaultLang = Object.keys(LANGUAGES)[0]
-const getKeyByValue = (object, value) => {
-  return Object.keys(object).find(key => object[key] === value);
-}
+const defaultLang = Object.keys(LANGUAGES)[0];
 
+function SearchForm({
+  initialKeyword = "",
+  initialRating = "g",
+  initialLang = defaultLang,
+}) {
+  const { keyword, rating, lang, updateKeyword, updateRating, updateLang } =
+    useForm({ initialKeyword, initialRating, initialLang });
+  const [, pushLocation] = useLocation();
 
-function SearchForm({initialKeyword='',initialRating='g',initialLang=defaultLang}) {
-  const {keyword,rating,lang,updateKeyword,updateRating,updateLang} = useForm({initialKeyword,initialRating,initialLang})
-  const [, pushLocation] = useLocation()
-  
-  const handleChange = event => {
-    updateKeyword(event.target.value)  
-  }
+  const handleChange = (event) => {
+    updateKeyword(event.target.value);
+  };
 
   const handleSubmit = (event, keyword) => {
-    event.preventDefault()
-    pushLocation(`/search/${keyword}/${rating}/${lang}`)
-  }
-  
-  const handleChangeRating = (event) =>{
-    updateRating(event.target.value)
+    event.preventDefault();
+    pushLocation(`/search/${keyword}/${rating}/${lang}`);
+  };
 
-  }
-  const handleChangeLang = (event) =>{
-    let value = event.target.value
-    let key = getKeyByValue(LANGUAGES,value)
-    console.log(key)
-    updateLang(key)
-
-  }
+  const handleChangeRating = (event) => {
+    updateRating(event.target.value);
+  };
+  const handleChangeLang = (event) => {
+    const key = event.target.selectedOptions[0].lang;
+    console.log(key);
+    updateLang(key);
+  };
 
   return (
-    <div className='form-wrap'>
-
-      <form className='search-form' onSubmit={(e) => handleSubmit(e, keyword)}>
-        <div className='input-wrap'>
-          <input placeholder="Buscar gifs" onChange={handleChange} type="text" value={keyword} />
-          <button className='search-button'>Buscar</button>
+    <div className="form-wrap">
+      <form className="search-form" onSubmit={(e) => handleSubmit(e, keyword)}>
+        <div className="input-wrap">
+          <input
+            placeholder="Buscar gifs"
+            onChange={handleChange}
+            type="text"
+            value={keyword}
+          />
+          <button className="search-button">Buscar</button>
         </div>
         <div className="filters-wrap">
-          <select className='filter' onChange={handleChangeRating} value={rating}>
+          <select
+            className="filter"
+            onChange={handleChangeRating}
+            value={rating}
+          >
             <option disabled>Rating type</option>
-            {RATINGS.map(rating => <option key={rating}>{rating}</option>)}
+            {RATINGS.map((rating) => (
+              <option key={rating}>{rating}</option>
+            ))}
           </select>
-          <select className='filter' onChange={handleChangeLang} value={LANGUAGES.lang}>
+          <select
+            className="filter"
+            onChange={handleChangeLang}
+            value={LANGUAGES[lang]}
+          >
             <option disabled>Language</option>
-            <option>{LANGUAGES.en}</option>
-            <option>{LANGUAGES.es}</option>
-            <option>{LANGUAGES.pt}</option>
+            {Object.entries(LANGUAGES).map(([codigo, nombre]) => (
+              <option lang={codigo}>{nombre}</option>
+            ))}
           </select>
         </div>
-
       </form>
-
     </div>
-  )
+  );
 }
 
-export default React.memo(SearchForm)
+export default React.memo(SearchForm);
