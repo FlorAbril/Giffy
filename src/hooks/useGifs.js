77a24a46cb570f0df9d0f350ae217c60f13f8a4f
FlorAbril <florenciasoto801@gmail.com>
2021-06-06 20:46:13 -0300

    Añandiendo boton volver atras en Detail

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,37 +1,38 @@
-import {useContext, useEffect,useState} from 'react'
-import getGifs from 'services/getGifs'
-import GifsContext from 'context/GifsContext'
+import { useContext, useEffect, useState } from "react";
+import getGifs from "services/getGifs";
+import GifsContext from "context/GifsContext";
 
-const INITIAL_PAGE = 0
-export function useGifs({keyword,rating,lang}={keyword:null}) {
-    const [loading, setLoading] = useState(false)
-    const [loadingNextPage, setLoadingNextPage] = useState(false)
-    const {gifs,setGifs} = useContext(GifsContext)
-    const [page,setPage] = useState(INITIAL_PAGE)
+const INITIAL_PAGE = 0;
+export function useGifs({ keyword, rating, lang } = { keyword: null }) {
+  const [loading, setLoading] = useState(false);
+  const [loadingNextPage, setLoadingNextPage] = useState(false);
+  const { gifs, setGifs } = useContext(GifsContext);
+  const [page, setPage] = useState(INITIAL_PAGE);
 
-    // recupero la keyword del local storage
-    const keywordToUse = keyword || localStorage.getItem('lastKeyword')
+  // recupero la keyword del local storage
+  const keywordToUse = keyword || localStorage.getItem("lastKeyword");
 
-    useEffect(function () {
-        setLoading(true)
-        getGifs({ keyword : keywordToUse , rating,lang})
-        .then(gifs => {
-            setGifs(gifs)
-            setLoading(false)
-            // guardo la keyword en el local storage
-            localStorage.setItem('lastKeyword', keywordToUse)
-            })
-    }, [keyword, keywordToUse, setGifs,rating,lang])
+  useEffect(
+    function () {
+      setLoading(true);
+      getGifs({ keyword: keywordToUse, rating, lang }).then((gifs) => {
+        setGifs(gifs);
+        setLoading(false);
+        // guardo la keyword en el local storage
+        if (keywordToUse) localStorage.setItem("lastKeyword", keywordToUse);
+      });
+    },
+    [keyword, keywordToUse, setGifs, rating, lang]
+  );
 
-    useEffect(()=>{
-        if(page === INITIAL_PAGE) return
-        setLoadingNextPage(true)
-        getGifs({keyword: keywordToUse, page,rating,lang})
-            .then(nextGifs =>{
-                 setGifs(prevGifs => prevGifs.concat(nextGifs))
-                 setLoadingNextPage(false)
-            })
-    },[keywordToUse, page,setGifs,rating,lang])
+  useEffect(() => {
+    if (page === INITIAL_PAGE) return;
+    setLoadingNextPage(true);
+    getGifs({ keyword: keywordToUse, page, rating, lang }).then((nextGifs) => {
+      setGifs((prevGifs) => prevGifs.concat(nextGifs));
+      setLoadingNextPage(false);
+    });
+  }, [keywordToUse, page, setGifs, rating, lang]);
 
-    return{loading,loadingNextPage,gifs,setPage,keywordToUse,lang}
+  return { loading, loadingNextPage, gifs, setPage, keywordToUse, lang };
 }
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,43 +1,47 @@
-import React, { useCallback } from 'react'
-import Gif from 'components/Gif'
-import useSingleGif from 'hooks/useSingleGif'
-import Spinner from 'components/Spinner'
-import { Redirect } from 'wouter'
-import {Helmet} from "react-helmet";
-import './styles.css'
-import useLocation from 'wouter/use-location'
+import React, { useCallback } from "react";
+import Gif from "components/Gif";
+import useSingleGif from "hooks/useSingleGif";
+import Spinner from "components/Spinner";
+import { Redirect } from "wouter";
+import { Helmet } from "react-helmet";
+import "./styles.css";
+import useLocation from "wouter/use-location";
 
+export default function Detail({ params }) {
+  const { gif, isLoading, isError } = useSingleGif({ id: params.id });
+  const title = gif ? gif.title : "";
 
-export default function Detail ({ params }) {
-  const {gif,isLoading,isError} = useSingleGif({id: params.id})
-  const title = gif ? gif.title : ''
+  const [, pushLocation] = useLocation();
 
-  const [, pushLocation] = useLocation()
-  
-  const handleSubmit = useCallback(() =>{
-    pushLocation(`/Giffy`)
-  },[pushLocation])
- 
+  const handleSubmit = useCallback(() => {
+    window.history.back();
+  }, []);
 
-  if(isLoading){ 
+  if (isLoading) {
     return (
       <>
-        <Helmet><title>Cargando...</title></Helmet>
-        <Spinner/>
+        <Helmet>
+          <title>Cargando...</title>
+        </Helmet>
+        <Spinner />
       </>
-  )}
+    );
+  }
 
-  if(isError) return <Redirect to='/404'/>
-  if(!gif) return null
+  if (isError) return <Redirect to="/404" />;
+  if (!gif) return null;
 
-  return (<>
-    <Helmet>
-      <title>{title} | Giffy</title>
-    </Helmet>
-    <div className='gif-wrap'>
-      <Gif {...gif}></Gif>
-      <button className='button' onClick={()=>handleSubmit()}>Back to home</button>
-    </div>
-  </>
-  )
+  return (
+    <>
+      <Helmet>
+        <title>{title} | Giffy</title>
+      </Helmet>
+      <div className="gif-wrap">
+        <Gif {...gif}></Gif>
+        <button className="button" onClick={() => handleSubmit()}>
+          Back
+        </button>
+      </div>
+    </>
+  );
 }
